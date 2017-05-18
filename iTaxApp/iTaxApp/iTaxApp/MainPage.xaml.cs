d14b299359b58787dfadefc6b2de4bb7321e51dc
Matej Mraz <matej.misura@gmail.com>
2017-05-18 11:14:15 +0200

    First commit

---
+++ iTaxApp/iTaxApp/iTaxApp/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Xamarin.Forms;
+
+namespace iTaxApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            StartClient();
+        }
+
+        public static void StartClient()
+        {
+            // Data buffer for incoming data.
+            byte[] bytes = new byte[1024];
+
+            Console.WriteLine("Start!");
+
+            // Connect to a remote device.
+            try
+            {
+                // Establish the remote endpoint for the socket.
+                // This example uses port 11000 on the local computer.
+
+
+                IPAddress ipAddress = new IPAddress(new byte[] { 86, 52, 212, 76 });
+                //   IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
+                Console.WriteLine("IP Created!");
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8113);
+                Console.WriteLine("Endpoint passed!");
+                // Create a TCP/IP  socket.
+                Socket sender = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+                Console.WriteLine("Socket created!");
+                // Connect the socket to the remote endpoint. Catch any errors.
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.
+                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                    // Send the data through the socket.
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // Release the socket.
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+
+                    Console.WriteLine("SocketException : {0}", se.ErrorCode);
+                    Console.WriteLine("SocketException : {0}", se.SocketErrorCode);
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
+
+
