1f24e2f6bccfb51186cc9f5edaf5b53c2f646533
Matej Mraz <matej.misura@gmail.com>
2017-05-22 16:10:01 +0200

    Prepared map to add custom stuff, not working properly yet..

--- iTaxApp/iTaxApp/iTaxApp/RidePage.xaml.cs
+++ iTaxApp/iTaxApp/iTaxApp/RidePage.xaml.cs
@@ -16,7 +16,21 @@ namespace iTaxApp
 		public RidePage ()
 		{
 			InitializeComponent ();
-            
+            var position = new Position(54.9134468, 9.7827599); // Latitude, Longitude
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = position,
+                Label = "Destination",
+                Address = "Order a taxi to this location."
+            };
+            MyMap.Pins.Add(pin);
+            /*customMap.RouteCoordinates.Add(new Position(37.785559, -122.396728));
+            customMap.RouteCoordinates.Add(new Position(37.780624, -122.390541));
+            customMap.RouteCoordinates.Add(new Position(37.777113, -122.394983));
+            customMap.RouteCoordinates.Add(new Position(37.776831, -122.394627));
+            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(37.79752, -122.40183), Distance.FromMiles(1.0)));
+            */
 
         }
         void OnExtras(object sender, EventArgs e)
